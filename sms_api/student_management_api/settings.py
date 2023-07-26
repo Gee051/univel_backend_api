495e839ffed96c1138f7d80747030af6943ac90a
Grace <otuaghagrace@gmail.com>
2023-07-26 16:00:31 +0100

    update

--- sms_api/student_management_api/settings.py
+++ sms_api/student_management_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!&1e@0-m-*^^4q$1k7ghyn@_@bnji8gl%(#*i9^an6mt_abvbd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -142,8 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR/ "static"]
-STATIC_ROOT= BASE_DIR/ "staticfiles"
+STATICFILES_DIRS=[BASE_DIR / "static"]
+STATIC_ROOT= BASE_DIR / "staticfiles"
 
 
 STORAGES = {
